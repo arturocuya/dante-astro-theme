bf1710e4a228ebcc3521a53291c03796228122cc
Arturo Cuya <arturocuya099@gmail.com>
2025-05-11 14:38:11 -0500

    fix urls

--- src/components/SveltePostPreview.svelte
+++ src/components/SveltePostPreview.svelte
@@ -4,6 +4,6 @@
 
 <article class="mb-8 p-4 border rounded">
   <h2 class="text-xl font-semibold">
-    <a href={`/blog/${post.slug}/`}>{post.title}</a>
+    <a href={`${post.slug}/`}>{post.title}</a>
   </h2>
 </article>
